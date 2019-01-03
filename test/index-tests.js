5ef2e93166bd373ae2a1b1e606dffe15af024be2
Parveen Sachdeva <sachdeva176@gmail.com>
2019-01-03 10:45:24 +0530

    Update index-tests.js

--- test/index-tests.js
+++ test/index-tests.js
@@ -1,28 +1,28 @@
 var expect = require("chai").expect;
-var CapitalizeFirstLetter = require('../index');
+var CapitalizeFirstLetterOfString = require('../index');
 
-describe('#CapitalizeFirstLetter()', function() {
+describe('#CapitalizeFirstLetterOfString()', function() {
   it('should return empty string for null param', function() {
-    let output = CapitalizeFirstLetter(null);
+    let output = CapitalizeFirstLetterOfString(null);
     expect(output).to.be.equals('');
   });
 
   it('should return empty string for undefined param', function() {
-    let output = CapitalizeFirstLetter(undefined);
+    let output = CapitalizeFirstLetterOfString(undefined);
     expect(output).to.be.equals('');
   });
 
   it('should return empty string for array/object param', function() {
     let array = new Array();
-    let output = CapitalizeFirstLetter(array);
+    let output = CapitalizeFirstLetterOfString(array);
     expect(output).to.be.equals('');
     let object = new Object();
-    output = CapitalizeFirstLetter(object);
+    output = CapitalizeFirstLetterOfString(object);
     expect(output).to.be.equals('');
   });
 
   it('should return correct value for any string as param', function() {
-    let output = CapitalizeFirstLetter('hello world');
+    let output = CapitalizeFirstLetterOfString('hello world');
     expect(output).to.be.equals('Hello world');
   });
 });
